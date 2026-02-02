58d505f457447a6c8caa8237b3552e7e699c6854
pancham01 <108358732+pancham01@users.noreply.github.com>
2026-02-02 13:42:11 +0530

    Initial commit

---
+++ src/main/java/my/jdbc/Main.java
@@ -0,0 +1,35 @@
+package my.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import my.jdbc.dao.EmployeeDao;
+import my.jdbc.dao.EmployeeDaoImpl;
+import my.jdbc.model.Employee;
+
+/*
+
+1st :- Create the Connection.
+
+Connection conn = DriverManager.getConnection("URL","username","password");
+Connection conn = DriverManager.getConnection("jdbc:mysql//localhost:3306/testdb","root","root");
+
+
+*/
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+
+		Employee e = new Employee(9, 44444, "Kunal singh", "kunal123@gmail.com");
+		String str="Ducat";
+
+		EmployeeDao empDao = new EmployeeDaoImpl();
+//		empDao.saveEmployee(e);
+		
+		empDao.updateEmployee(e);
+	
+	}
+
+}
---
+++ src/main/java/my/jdbc/dao/EmployeeDaoImpl.java
@@ -0,0 +1,82 @@
+package my.jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import my.jdbc.model.Employee;
+
+public class EmployeeDaoImpl implements EmployeeDao {
+	
+	
+	public static final String UPDATE_QUERY = "update employee set name = '%s' , email = '%s', salary = %d where empId = %d";
+
+	private static Connection connection = null;
+
+	static {
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "root");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void saveEmployee(Employee e) throws SQLException {
+
+		Statement statement = connection.createStatement();
+//		statement.executeUpdate("insert into employee (empId,name,email,salary) values(8,'Puneet','john@gmail.com',88888)");
+		statement.executeUpdate("insert into employee (empId,name,email,salary) values(" + e.getId() + ",'"+ e.getName() + "','" + e.getEmail() + "'," + e.getSalary() + ")");
+		System.out.println("insert into employee (empId,name,email,salary) values(" + e.getId() + ",'" + e.getName()+ "','" + e.getEmail() + "'," + e.getSalary() + ")");
+	}
+
+	@Override
+	public void updateEmployee(Employee e) {
+		
+		
+		try(Statement statement = connection.createStatement()) {
+			
+			statement.executeUpdate(String.format(UPDATE_QUERY, e.getName(),e.getEmail(),e.getSalary(),e.getId()));
+			
+			System.out.println(String.format(UPDATE_QUERY, e.getName(),e.getEmail(),e.getSalary(),e.getId()));
+			
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		
+
+	}
+
+	@Override
+	public void deleteAnEmployee(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printAllEmployee() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Employee getEmpById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAllEmps() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Employee getEmpByName(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
