bd904d9bedad2a0d173a726aa22ce17dbe7f3cd2
pancham01 <108358732+pancham01@users.noreply.github.com>
2026-02-04 14:07:40 +0530

    update

--- src/main/java/my/jdbc/dao/EmployeeDaoImpl.java
+++ src/main/java/my/jdbc/dao/EmployeeDaoImpl.java
@@ -31,25 +31,11 @@ public class EmployeeDaoImpl implements EmployeeDao {
 	@Override
 	public void saveEmployee(Employee e) throws SQLException {
 
-		Statement statement = connection.createStatement();
-		statement.executeUpdate("insert into employee (empId,name,email,salary) values(" + e.getId() + ",'"
-				+ e.getName() + "','" + e.getEmail() + "'," + e.getSalary() + ")");
-		System.out.println("insert into employee (empId,name,email,salary) values(" + e.getId() + ",'" + e.getName()
-				+ "','" + e.getEmail() + "'," + e.getSalary() + ")");
 	}
 
 	@Override
 	public void updateEmployee(Employee e) {
 
-		try (Statement statement = connection.createStatement()) {
-
-			statement.executeUpdate(String.format(UPDATE_QUERY, e.getName(), e.getEmail(), e.getSalary(), e.getId()));
-
-			System.out.println(String.format(UPDATE_QUERY, e.getName(), e.getEmail(), e.getSalary(), e.getId()));
-
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
 
 	}
 
